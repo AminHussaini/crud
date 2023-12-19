0afa45b50ba523c43fd7f713ef9f6cdd60d9c9f4
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-12-19 19:15:40 +0500

    first commit

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB, {
+    await mongoose.connect(`${process.env.MONGO_DB}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    
+
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
