47b008e5dca6e159de84a4fb9c9d50906099378b
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-12-19 14:15:33 +0500

    Initial commit from Create Next App

---
+++ src/models/Task.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const taskSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  hour: { type: String, required: true },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
