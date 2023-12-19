4eb963adb59789befc823079a75d50049ae392d8
syed-amin-hussaini <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-12-19 19:22:43 +0500

    first commit

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB);
+    await mongoose.connect(`${process.env.MONGO_DB}`);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
